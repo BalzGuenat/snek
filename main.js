cbe03daf0565330c01b0e1d77c151463e5151737
Balz Guenat <balz.guenat@gmail.com>
2019-11-29 21:19:07 +0100

    rebuild with canvas, add game over condition

--- main.js
+++ main.js
@@ -5,10 +5,11 @@ maze_w = 20;
 maze_h = 10;
 cell_size = 0;
 state = {
-	snake: [{x: 0, y: 0}],
+	snake: [{x: 3, y: 1}, {x: 2, y: 1}, {x: 1, y: 1}],
 	target: {x: 5, y: 5},
 	board: [],
-	to_grow: 0
+	to_grow: 0,
+	game_over: false
 };
 changed = [];
 
@@ -19,14 +20,6 @@ const S_TARGET = 2;
 function main(event) {
 	initState();
 	drawMaze();
-
-
-	// markCell(1, 2);
-	// state.board[1][2] = S_TARGET;
-	// changed.push({x:1, y:2});
-	// state.board[1][1] = S_SNAKE;
-	// changed.push({x:1, y:1});
-	// updateMaze();
 }
 
 function initState() {
@@ -44,23 +37,15 @@ function initState() {
 }
 
 function drawMaze() {
-	var w = window.innerWidth;
-	var h = window.innerHeight;
-	cell_size = Math.min(w/maze_w, h/maze_h);
+	var w = window.innerWidth * .95;
+	var h = window.innerHeight * .95;
+	cell_size = Math.floor(Math.min(w/maze_w, h/maze_h));
 
 	var maze = document.getElementById('maze');
+	maze.width = cell_size * maze_w;
+	maze.height = cell_size * maze_h;
 	for (var i = 0; i < maze_h; i++) {
-		var row = document.createElement('tr');
-		row.classList.add('row');
-		row.id = 'r' + i;
-		maze.appendChild(row);
 		for (var j = 0; j < maze_w; j++) {
-			var cell = document.createElement('td');
-			cell.setAttribute('width', cell_size);
-			cell.setAttribute('height', cell_size);
-			cell.classList.add('cell');
-			cell.id = id(j, i);
-			row.appendChild(cell);
 			changed.push({x:j, y:i});
 		}
 	}
@@ -68,36 +53,60 @@ function drawMaze() {
 }
 
 function updateMaze() {
-	for (var i = 0; i < changed.length; i++) {
-		var c = changed[i];
-		var s = state.board[c.x][c.y];
-		if (s == S_SNAKE) {
-			markCell(c.x, c.y, 'green');
-		} else if (s == S_TARGET) {
-			markCell(c.x, c.y, 'red');
-		} else {
-			clearCell(c.x, c.y);
-		}
+	var maze = document.getElementById('maze');
+	var ctx = maze.getContext('2d');
+	ctx.clearRect(0, 0, maze.width, maze.height);
+	var t = state.target;
+	markCell(t.x, t.y, 'red');
+
+	var head_dx = state.snake[0].x - state.snake[1].x;
+	var head_dy = state.snake[0].y - state.snake[1].y;
+	var tail_dx = state.snake[state.snake.length-2].x - state.snake[state.snake.length-1].x;
+	var tail_dy = state.snake[state.snake.length-2].y - state.snake[state.snake.length-1].y;
+
+	ctx.beginPath();
+	if (state.game_over) {
+		ctx.strokeStyle = 'orange';
+	} else {
+		ctx.strokeStyle = 'green';
 	}
-	changed = [];
+	ctx.lineWidth = cell_size * .8;
+	var start = center(state.snake[0].x, state.snake[0].y);
+	start = {
+		x: start.x + cell_size * .4 * head_dx,
+		y: start.y + cell_size * .4 * head_dy
+	};
+	ctx.moveTo(start.x, start.y);
+	for (var i = 1; i < state.snake.length - 1; i++) {
+		var next = center(state.snake[i].x, state.snake[i].y);
+		ctx.lineTo(next.x, next.y);
+	}
+	var end = center(state.snake[state.snake.length-1].x, state.snake[state.snake.length-1].y);
+	end = {
+		x: end.x - cell_size * .4 * tail_dx,
+		y: end.y - cell_size * .4 * tail_dy
+	};
+	ctx.lineTo(end.x, end.y);
+	ctx.stroke();
 }
 
 function markCell(x, y, color) {
-	var div = document.createElement('div');
-	div.style.height = cell_size*.9 + 'px';
-	div.style.width = cell_size*.9 + 'px';
-	div.style['background-color'] = color;
-	var c = cell(x, y);
-	c.innerHTML = "";
-	c.appendChild(div)
+	var ctx = document.getElementById('maze').getContext('2d');
+	ctx.fillStyle = color;
+	ctx.fillRect(x * cell_size + cell_size * .25, y * cell_size + cell_size * .25, cell_size * .5, cell_size * .5);
 }
 
 function clearCell(x, y) {
-	cell(x, y).innerHTML = "";
+	var c = document.getElementById('maze').getContext('2d');
+	c.fillStyle = 'black';
+	c.fillRect(x*cell_size, y*cell_size, cell_size, cell_size);
 }
 
-function cell(x, y) {
-	return document.getElementById(id(x, y));
+function center(x, y) {
+	return {
+		x: x * cell_size + cell_size * .5, 
+		y: y * cell_size + cell_size * .5
+	};
 }
 
 function id(x, y) {
@@ -124,15 +133,6 @@ function move(dx, dy) {
 		return;	
 	}
 
-	if (state.board[newpos.x][newpos.y] == S_TARGET) {
-		state.to_grow += 2;
-		do {
-			newtarget = {x:getRandomInt(0, maze_w), y:getRandomInt(0, maze_h)};
-		} while (state.board[newtarget.x][newtarget.y] != S_NOTHING);
-		state.board[newtarget.x][newtarget.y] = S_TARGET;
-		changed.push(newtarget);
-	}
-
 	state.snake.unshift(newpos);
 	if (state.to_grow > 0) {
 		state.to_grow -= 1;
@@ -141,8 +141,22 @@ function move(dx, dy) {
 		state.board[old.x][old.y] = S_NOTHING;
 		changed.push(old);
 	}
+
+	if (state.board[newpos.x][newpos.y] == S_TARGET) {
+		state.to_grow += 2;
+		do {
+			newtarget = {x:getRandomInt(0, maze_w), y:getRandomInt(0, maze_h)};
+		} while (state.board[newtarget.x][newtarget.y] != S_NOTHING);
+		state.board[newtarget.x][newtarget.y] = S_TARGET;
+		state.target = newtarget;
+		changed.push(newtarget);
+	}
+
 	state.board[newpos.x][newpos.y] = S_SNAKE;
 	changed.push(state.snake[0]);
+
+	checkGameOver();
+
 	updateMaze();
 }
 
@@ -151,4 +165,17 @@ function getRandomInt(min, max) {
   max = Math.floor(max);
   //The maximum is exclusive and the minimum is inclusive
   return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function eq(a, b) {
+	return a.x == b.x && a.y == b.y;
+}
+
+function checkGameOver() {
+	var head = state.snake[0];
+	var rightBlocked = head.x + 1 == maze_w || state.board[head.x + 1][head.y] == S_SNAKE;
+	var leftBlocked = head.x  == 0 || state.board[head.x - 1][head.y] == S_SNAKE;
+	var downBlocked = head.y + 1 == maze_h || state.board[head.x][head.y + 1] == S_SNAKE;
+	var upBlocked = head.y  == 0 || state.board[head.x][head.y - 1] == S_SNAKE;
+	state.game_over = rightBlocked && leftBlocked && downBlocked && upBlocked;
 }
